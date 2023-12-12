06abc66fafe67f2c08bca055deec55e56a8911bc
Ruben Bouwman <129963248+rubenbouwman@users.noreply.github.com>
2023-12-12 17:54:42 +0100

    Build

--- obj/Debug/net6.0/ContosoPizza.AssemblyInfo.cs
+++ obj/Debug/net6.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c11a7ef21e7df45217e0aefa756422be0d80620")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
